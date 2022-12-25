bfcd709c8dfb45dd5216ea5c9ac7fc1144e3edb8
vic778 <victoiremmanuelbarh@gmail.com>
2022-12-25 14:19:38 +0400

    Create collbacks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+
+    after_create :publish_job
+    after_update :publish_job
+
+    def publish
+        update(published: true, schedule: false)
+    end
+
+    private
+
+    def publish_job
+        puts "================================="
+        if self.publish_at < Time.now
+            self.publish
+        else
+            puts "================================next"
+            PostPublishScheduler.perform_at(self.publish_at, self.id)
+        end
+    end
 end
